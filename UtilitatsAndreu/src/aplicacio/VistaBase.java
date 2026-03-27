e1201830b52c51921abdbbc8fc93602021e07c92
andreu <agarriga18696@iesjoanramis.org>
2026-03-27 17:02:43 +0100

    afegit package aplicacio amb la classe VistaBase.java

---
+++ UtilitatsAndreu/src/aplicacio/VistaBase.java
@@ -0,0 +1,45 @@
+package aplicacio;
+
+import utilitats.Menu;
+
+/**
+ * Classe base per crear la classe Vista.
+ * 
+ * @author Andreu
+ * @version 1.0
+ */
+
+public abstract class VistaBase {
+
+	////////////////////////////////////////////////////
+	/// ATRIBUTS ABSTRACTES
+	////////////////////////////////////////////////////
+
+	protected abstract String titol();
+	protected abstract String[] opcions();
+	protected abstract void gestionar(int opcio);
+
+	////////////////////////////////////////////////////
+	/// MÈTODES DE LA CLASSE
+	////////////////////////////////////////////////////
+
+	/**
+	 * Mostra el menú principal de l'aplicació amb el títol i les opcions.
+	 * <p>
+	 * Funcions:
+	 * <ul>
+	 * <li>Demana per consola una opció {@code int} fins que introdueixi l'opció per sortir</li>
+	 * <li>Gestiona l'opció introduïda per l'usuari</li>
+	 * </ul>
+	 * <p>
+	 * <i><b>Nota:</b> l'opció per sortir sempre és la darrera de l'array d'opcions.</i>
+	 */
+	public void menu() {
+		int opcio;
+		do {
+			opcio = Menu.mostrar(titol(), opcions());
+			gestionar(opcio);
+		} while(opcio != opcions().length);
+	}
+
+}
