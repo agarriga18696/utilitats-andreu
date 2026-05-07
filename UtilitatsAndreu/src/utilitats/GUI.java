c32b82bc6a7e0d30d51a4625d5ff8a70727489c1
andreu <agarriga18696@iesjoanramis.org>
2026-05-07 21:15:16 +0200

    feat(GUI): afegits molts mètodes inicials

--- UtilitatsAndreu/src/utilitats/GUI.java
+++ UtilitatsAndreu/src/utilitats/GUI.java
@@ -1,7 +1,8 @@
 package utilitats;
 
-import javax.swing.Icon;
-import javax.swing.JButton;
+import java.awt.*;
+
+import javax.swing.*;
 
 /**
  * Classe d'utilitats per a components d'interfície gràfica.
@@ -11,22 +12,136 @@ import javax.swing.JButton;
  */
 
 public final class GUI {
-	
+
 	private GUI() {}
-
-	////////////////////////////////////////////////////
-	/// CONTENIDORS
-	////////////////////////////////////////////////////
-
-	////////////////////////////////////////////////////
-	/// ELEMENTS INTERACTIUS
-	////////////////////////////////////////////////////
+	
+	/*
+	 * CONTENIDORS
+	 */
 
 	/**
-	 * Retorna un JButton amb un títol i icona.
+	 * Retorna un JFrame configurat amb els paràmetres indicats.
 	 */
-	public static final JButton boto(String titol, Icon icona) {
-		return icona != null ? new JButton(titol, icona) : new JButton(titol);
+	public static final JFrame frame(
+			String title, 
+			int width, 
+			int height, 
+			Component c,
+			int operation) {
+		
+		if(width == 0 || height == 0) return null;
+
+		JFrame frame = new JFrame(title);
+
+		frame.setSize(width, height);
+		frame.setLocationRelativeTo(c);
+		frame.setDefaultCloseOperation(operation);
+
+		return frame;
 	}
 	
+	/**
+	 * Retorna un JPanel amb el Layout indicat per paràmetre.
+	 */
+	public static final JPanel panel(LayoutManager layout) {
+		return new JPanel(layout);
+	}
+	
+	/**
+	 * Retorna un JPanel amb GridLayout i les files i columnes especificades.
+	 */
+	public static final JPanel panelGrid(int rows, int cols) {
+		if(rows == 0 || cols == 0) return null;
+		return new JPanel(new GridLayout(rows, cols));
+	}
+	
+	/**
+	 * Retorna un JPanel amb GridLayout i les files, columnes i gap especificats.
+	 */
+	public static final JPanel panelGrid(int rows, int cols, int hgap, int vgap) {
+		if(rows == 0 || cols == 0) return null;
+		return new JPanel(new GridLayout(rows, cols, hgap, vgap));
+	}
+
+	/*
+	 * COMPONENTS
+	 */
+	
+	/**
+	 * Retorna un JLabel amb el text.
+	 */
+	public static final JLabel label(String text) {
+		return new JLabel(text);
+	}
+
+	/**
+	 * Retorna un JLabel amb el text i alineació.
+	 */
+	public static final JLabel label(String text, int horizontalAlignment) {
+		return new JLabel(text, horizontalAlignment);
+	}
+	
+	/**
+	 * Retorna un JButton amb un text.
+	 */
+	public static final JButton button(String text) {
+		return new JButton(text);
+	}
+	
+	/*
+	 * FUNCIONALITATS
+	 */
+	
+	/**
+	 * Afegeix un component al {@code panel} i el retorna.
+	 */
+	public static final JPanel add(JPanel panel, Component comp) {
+		JPanel pnl = panel;
+		pnl.add(comp);
+		return pnl;
+	}
+	
+	/**
+	 * Afegeix els components passats per paràmetre al {@code panel} i el retorna.
+	 */
+	public static final JPanel add(JPanel panel, Component... comp) {
+		JPanel pnl = panel;
+		for(Component c : comp) {
+			pnl.add(c);
+		}
+		return pnl;
+	}
+	
+	/**
+	 * Retorna un JButton de dins un JComponent.
+	 */
+	public static final JButton getButton(JComponent jcomp, int n) {
+		if(!(jcomp.getComponent(n) instanceof JButton _)) return null;
+		return (JButton) jcomp.getComponent(n);
+	}
+	
+	/**
+	 * Retorna un JPanel amb GridLayout, les files i columnes especificades i farcit amb el JComponent especificat.
+	 */
+	public static final JPanel panelGridComp(int rows, int cols, JComponent jcomp) {
+		if(rows == 0 || cols == 0) return null;
+		JPanel panel = new JPanel(new GridLayout(rows, cols));
+		for(int i = 0; i < rows * cols; i++) {
+			panel.add(jcomp);
+		}
+		return panel;
+	}
+	
+	/**
+	 * Retorna un JPanel amb GridLayout, les files i columnes especificades, gaps i farcit amb el JComponent especificat.
+	 */
+	public static final JPanel panelGridComp(int rows, int cols, int hgap, int vgap, JComponent jcomp) {
+		if(rows == 0 || cols == 0) return null;
+		JPanel panel = new JPanel(new GridLayout(rows, cols, hgap, vgap));
+		for(int i = 0; i < rows * cols; i++) {
+			panel.add(jcomp);
+		}
+		return panel;
+	}
+
 }
