21973971fec3602447ba2bdb1dc693595f8ebc80
Andreu Garriga Cendán <agarriga18696@iesjoanramis.org>
2026-08-27 16:44:12 +0200

    Configure application language

--- src/main/java/io/github/agarriga18696/andreuutils/application/ApplicationBase.java
+++ src/main/java/io/github/agarriga18696/andreuutils/application/ApplicationBase.java
@@ -1,31 +1,50 @@
 package io.github.agarriga18696.andreuutils.application;
 
+import io.github.agarriga18696.andreuutils.core.Language;
+import io.github.agarriga18696.andreuutils.core.LanguageManager;
+
 /**
  * Base class for creating console applications.
  *
  * @author Andreu
- * @version 2.0
+ * @version 2.1
  */
 public abstract class ApplicationBase {
 
-	////////////////////////////////////////////
-	/// ABSTRACT METHODS
-	////////////////////////////////////////////
+    /// /////////////////////////////////////////
+    /// ABSTRACT METHODS
+    /// /////////////////////////////////////////
 
-	protected abstract ControllerBase controller();
-	protected abstract ViewBase view(ControllerBase controller);
+    protected abstract ControllerBase controller();
 
-	////////////////////////////////////////////
-	/// PUBLIC METHODS
-	////////////////////////////////////////////
+    protected abstract ViewBase view(ControllerBase controller);
 
-	/**
-	 * Runs the application.
-	 */
-	public void run() {
-		ControllerBase controller = controller();
-		controller.initialize();
-		view(controller).showMenu();
-	}
+    ////////////////////////////////////////////
+    /// CONFIGURATION
+    ////////////////////////////////////////////
+
+    /**
+     * Returns the initial language used by the application.
+     *
+     * @return The application language.
+     */
+    protected Language language() {
+        return Language.ENGLISH;
+    }
+
+    ////////////////////////////////////////////
+    /// PUBLIC METHODS
+    ////////////////////////////////////////////
+
+    /**
+     * Runs the application.
+     */
+    public void run() {
+        LanguageManager.setLanguage(language());
+
+        ControllerBase controller = controller();
+        controller.initialize();
+        view(controller).showMenu();
+    }
 
 }
